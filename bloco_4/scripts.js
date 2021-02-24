5bd82be588309417c7ad97d1ad6ddda76aa0b918
Samuel Melo <samuelmmjunior@gmail.com>
2021-02-24 18:59:48 -0300

    exercicio introdução js

---
+++ bloco_4/scripts.js
@@ -0,0 +1,15 @@
+let nota = 30;
+
+if(nota >= 80){
+    console.log("Parabéns, você foi aprovado(a)!");
+}
+else if(nota < 80 && nota >= 60){
+    console.log("Você está na nossa lista de espera");
+}
+else{
+    console.log("Você foi reprovada(o)");
+}
+
+let msg = console.log()
+
+console.log(msg);
