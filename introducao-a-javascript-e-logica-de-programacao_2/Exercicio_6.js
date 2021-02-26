0019b928a17bbc88bc0f5bf0dda79cb74430d85b
Samuel Melo <samuelmmjunior@gmail.com>
2021-02-26 19:43:20 -0300

    resolução exexercicio 1 bloco 4.3

--- introducao-a-javascript-e-logica-de-programacao_2/Exercicio_6.js
+++ introducao-a-javascript-e-logica-de-programacao_2/Exercicio_6.js
@@ -1,9 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let MaiorValor = 0;
+let NumImpar = [];
 
 for(let index = 0; index < numbers.length; index ++){
-    if ( numbers[index] > MaiorValor ) {
-        MaiorValor = numbers[index];
+    if ( numbers[index] % 2 != 0) {
+        NumImpar.push = numbers[index];
      }
     }
-     console.log (MaiorValor)
+     console.log (NumImpar[index])
